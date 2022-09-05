8b6c0889953fe193b8f89883a998f653c3a5b1f7
FEIYU YIN <105698932+Canon70D@users.noreply.github.com>
2022-09-05 20:24:27 +0930

    category route updated

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,163 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const cateData = await Category.findAll({
+      include: { model: Product },
+    });
+    res.status(200).json(cateData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+  // Category.findAll({
+  //   include: {
+  //     model: Product,
+  //   },
+  // })
+  //   .then((cateData) => res.json(cateData))
+  //   .catch((err) => {
+  //     console.log(err);
+  //     res.status(500).json(err);
+  //   });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const cateData = await Category.findByPk(req.params.id, {
+      include: { model: Product },
+    });
+    res.status(200).json(cateData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+  // Category.findOne({
+  //   where: {
+  //     id: req.params.id,
+  //   },
+  //   include: {
+  //     model: Product,
+  //   },
+  // })
+  //   .then((cateData) => {
+  //     if (!cateData) {
+  //       res
+  //         .status(404)
+  //         .json({ message: "Not a valid category ID" });
+  //       return;
+  //     }
+  //     res.json(cateData);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //     res.status(500).json(err);
+  //   });
 });
 
-router.post('/', (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  try {
+    const cateData = await Category.create(req.body);
+    res.status(200).json(cateData);
+  } catch (err) {
+    res.status(400).json(err);
+
+    // Category.create({
+    //   category_name: req.body.category_name
+    // })
+    //   .then(cateData => res.json(cateData))
+    //   .catch(err => {
+    //     console.log(err);
+    //     res.status(500).json(err);
+    //   });
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const cateData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    console.log(cateData);
+    if (!cateData) {
+      res.status(404).json({ message: "Not a valid category ID" });
+      return;
+    }
+
+    res.status(200).json(cateData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+  // Category.update(
+  //   {
+  //     category_name: req.body.category_name,
+  //   },
+  //   {
+  //     where: {
+  //       id: req.params.id,
+  //     },
+  //   }
+  // )
+  //   .then((cateData) => {
+  //     if (!cateData) {
+  //       res.status(404).json({ message: "Not a valid category ID" });
+  //       return;
+  //     }
+  //     res.json(cateData);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //     res.status(500).json(err);
+  //   });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const cateData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!cateData) {
+      res.status(404).json({ message: "Not a valid category ID" });
+      return;
+    }
+
+    res.status(200).json(cateData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+  // Category.destroy({
+  //   where: {
+  //     id: req.params.id,
+  //   },
+  // })
+  //   .then((dbPostData) => {
+  //     if (!dbPostData) {
+  //       res.status(404).json({ message: "Not a valid category ID" });
+  //       return;
+  //     }
+  //     res.json(dbPostData);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //     res.status(500).json(err);
+  //   });
 });
 
 module.exports = router;
