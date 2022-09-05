2f565a3b6856d4e785db9526f018754a4c5038d5
FEIYU YIN <105698932+Canon70D@users.noreply.github.com>
2022-09-05 23:07:08 +0930

    tag route updated by using same method from product route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -41,7 +41,7 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
+router.post("/", (req, res) => {
   // create a new tag
   /* req.body should look like this...
     {
@@ -49,32 +49,64 @@ router.post("/", async (req, res) => {
       "productIds": [1, 2, 3]
     }
   */
-  try {
-    const tData = await Tag.create({
-      tag_name: req.body.tag_name,
+
+  //use same method that provided in product-routes
+  Tag.create(req.body)
+    .then((tag) => {
+      if (req.body.productIds.length) {
+        const productTagIdArr = req.body.productIds.map((product_id) => {
+          return {
+            tag_id: tag.id,
+            product_id,
+          };
+        });
+        return ProductTag.bulkCreate(productTagIdArr);
+      }
+      res.status(200).json(tag);
+    })
+    .then((productTagIds) => res.status(200).json(productTagIds))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
     });
-    res.status(200).json(tData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
-  try {
-    const tData = await Tag.update(req.body.tag_name, {
-      where: {
-        id: req.params.id,
-      },
+
+  //use same method that provided in product-routes
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => {
+      return ProductTag.findAll({ where: { tag_id: req.params.id } });
+    })
+    .then((productTags) => {
+      const productTagIds = productTags.map(({ product_id }) => product_id);
+      const newProductTags = req.body.productIds
+        .filter((product_id) => !productTagIds.includes(product_id))
+        .map((product_id) => {
+          return {
+            tag_id: req.params.id,
+            product_id,
+          };
+        });
+
+      const productTagsToRemove = productTags
+        .filter(({ product_id }) => !req.body.productIds.includes(product_id))
+        .map(({ id }) => id);
+
+      return Promise.all([
+        ProductTag.destroy({ where: { id: productTagsToRemove } }),
+        ProductTag.bulkCreate(newProductTags),
+      ]);
+    })
+    .then((updatedProductTags) => res.json(updatedProductTags))
+    .catch((err) => {
+      res.status(400).json(err);
     });
-    if (!tData) {
-      res.status(404).json({ message: "not a valid tag ID" });
-      return;
-    }
-    res.json(tData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
 
 router.delete("/:id", async (req, res) => {
